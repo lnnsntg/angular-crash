4686c656dcb5074ac1bdfc5d12d69d671d853d25
Lenin Santiago <lnnsntg@gmail.com>
2023-01-09 18:41:15 +0100

    added service to tasks component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { TASKS } from "../../mock-tasks";
-import { Task } from "../../Task";
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
-tasks : Task[] = TASKS
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.geTask();
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { TASKS } from "../mock-tasks";
+import { Task } from "../Task";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TaskService {
 
   constructor() { }
+
+  geTask(): Task[] {
+    return TASKS
+  }
 }
